c77f7397ab93660c967bbf96fb5a97b1bf9b676b
atacanyavuz <atacanyavuz99@gmail.com>
2025-05-15 04:32:47 +0300

    Initial commit

---
+++ backend/ticketing-system/src/test/java/com/atacanyavuz/ticketing/TicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atacanyavuz.ticketing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
